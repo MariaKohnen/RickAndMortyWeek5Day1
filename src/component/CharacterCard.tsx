976e63b7451dbd04464a6024ac35836336fd204e
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-04-14 23:24:47 +0200

    Image alternative text gives now "Picture of " and then the character name.

--- src/component/CharacterCard.tsx
+++ src/component/CharacterCard.tsx
@@ -7,7 +7,7 @@ type CharacterProps = {
 
 export default function CharacterCard({character} : CharacterProps) {
     return <div id="CharacterCard"><div className="characterText">{character.name}</div>
-        <img src={character.image} alt={character.image}/>
+        <img src={character.image} alt={"Picture of " + character.name}/>
         <div className="characterText">{character.location.name}</div>
     </div>
 }
