86daff359dab3900861b598a8bec4200bb5e57a4
LutzWerner <lutz@werner.de>
2022-04-14 11:14:23 +0200

    title created

---
+++ src/component/Title.tsx
@@ -0,0 +1,8 @@
+import "./Title.css";
+
+export default function Title() {
+    return <header>
+            <img alt="Rick and Morty Icon" src="https://rickandmortyapi.com/icons/icon-512x512.png" />
+            <h1>Rick and Morty</h1>
+            </header>
+}
