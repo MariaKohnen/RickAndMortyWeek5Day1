1ab4961eb5b853d1906d8f4a5b4a78bb1c4be9a3
Maria Kohnen <maria.kohnen@gmail.com>
2022-04-14 12:53:01 +0200

    add CharacterCard

---
+++ src/component/CharacterCard.tsx
@@ -0,0 +1,9 @@
+import {Character} from "../model/Character";
+
+type CharacterProps = {
+    character : Character;
+}
+
+export default function CharacterCard({character} : CharacterProps) {
+    return <div>Text</div>
+}
