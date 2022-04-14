6ffcd22da7f4f52965c6c4d8512293bb79c5c76c
Stefan Bollmann <stefan.bollmann@rwth-aachen.de>
2022-04-14 13:57:42 +0200

    CharacterCard.tsx displays now content: name, picture, and location.
    Todo: Layout

--- src/component/CharacterCard.tsx
+++ src/component/CharacterCard.tsx
@@ -4,6 +4,11 @@ type CharacterProps = {
     character : Character;
 }
 
+
+
 export default function CharacterCard({character} : CharacterProps) {
-    return <div>Text</div>
+    return <div><div>{character.name}</div>
+        <img src={character.image} alt={character.image}/>
+        <div>{character.location.name}</div>
+    </div>
 }
