b60a96791e99b40c02a401c0d09865aa22fc2263
Maria Kohnen <maria.kohnen@gmail.com>
2022-04-19 19:25:23 +0200

    move API to own folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,55 @@
-import React from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import './App.css';
 import Title from "./component/Title";
-import {exportedDatas} from "./repo/Datas";
 import Gallery from "./component/Gallery";
+import {Character} from "./model/Character";
 
 export default function App() {
-  return (
-    <div>
 
-      <Title />
-        <Gallery characters = {exportedDatas} />
+    const [count, setCount] = useState<string>("");
+    const [text, setText] = useState<string>("");
+    const [characters, setCharacters] = useState<Character[]>([]);
 
-    </div>
-  );
+
+    const fetchCharacters = () => {
+        return fetch('https://rickandmortyapi.com/api/character')
+            .then(response => {
+                if (response.ok) {
+                    return response.json()
+                }
+                throw new Error("Network error")
+            })
+            .catch(console.error)
+    }
+
+    useEffect(() => {
+        fetchCharacters()
+            .then(body => setCharacters(body.results))
+    }, [])
+
+    const onButtonClick = () => {
+        setCount(text);
+    }
+
+    const onTextChange = (event: ChangeEvent<HTMLInputElement>) => {
+        console.log(event.target.value);
+        setText(event.target.value);
+    }
+
+    const filterCharacter = characters.filter(obj => obj.name.toLocaleLowerCase().includes(count.toLocaleLowerCase()));
+
+    return (
+        <div>
+            <Title/>
+            <div><input onChange={onTextChange}/>
+                <button onClick={onButtonClick}>Charackter filtern</button>
+            </div>
+            {filterCharacter.length
+                ? <Gallery characters={filterCharacter}/>
+                : <div>Deine Suche ergab kein Ergebnis
+                    <Gallery characters={characters}/></div>
+            }
+        </div>
+    );
 }
 
--- src/component/CharacterCard.tsx
+++ src/component/CharacterCard.tsx
@@ -1,14 +1,30 @@
 import {Character} from "../model/Character";
 import "./CharacterCard.css"
+import React, {useState} from "react";
 
 type CharacterProps = {
-    character: Character;
+    character : Character;
 }
 
-export default function CharacterCard({character}: CharacterProps) {
-    return <div id="CharacterCard">
-        <div className="characterText">{character.name}</div>
-        <img src={character.image} alt={character.image}/>
-        <div>{character.location.name}</div>
+
+export default function CharacterCard({character} : CharacterProps) {
+/*
+    const [count, setCount] = useState<number>(0);
+
+    console.log(count);
+
+    const onButtonClick = () => {
+        console.log("Click me!");
+        setCount(count + 1);
+    }
+
+ */
+    return <div id="CharacterCard"><div className="characterText">{character.name}</div>
+        <img src={character.image} alt={"Picture of " + character.name}/>
+        <div className="characterText">{character.location.name}</div>
+
+
     </div>
 }
+
+//<div><button onClick={onButtonClick}>{count} likes</button></div>
